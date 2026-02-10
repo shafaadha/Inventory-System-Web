7de2c4918c5e729e99108535fb31fd0702e4aad1
shafaadha <adhashafa@gmail.com>
2026-02-10 12:25:38 +0700

    Update report routes

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,10 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\ReportController;
 use App\Http\Controllers\StockInController;
 use App\Http\Controllers\StockOutController;
 use App\Http\Controllers\TransactionController;
@@ -25,20 +27,17 @@ Route::get('/', function () {
 
 Route::get('/login', [AuthController::class, 'showLogin'])->name('login');
 Route::post('/login', [AuthController::class, 'login'])->name('login.process');
-Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
-
-Route::middleware('auth')->group(function () {
-    Route::get('/dashboard', function () {
-        return view('dashboard');
-    })->name('dashboard');
-});
+Route::post('/logout', [AuthController::class, 'logout'])->name('logout');;
 
 Route::middleware(['auth'])->group(function () {
+    Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
     Route::resource('products', ProductController::class);
-    Route::resource('stock-in', StockInController::class);
-    Route::resource('stock-out', StockOutController::class);
+    Route::resource('stock-in', StockInController::class)->names('stock_in');
+    Route::resource('stock-out', StockOutController::class)->names('stock_out');
     Route::get('transaction', [TransactionController::class, 'index'])
         ->name('transaction.index');
-    //     Route::get('laporan/stok', [LaporanController::class, 'stok'])->name('laporan.stok');
-    // Route::get('laporan/transaksi', [LaporanController::class, 'transaksi'])->name('laporan.transaksi');
+    Route::get('/report/stock', [ReportController::class, 'stock'])
+        ->name('report.stock');
+    Route::get('/report/transaction', [ReportController::class, 'transaction'])
+        ->name('report.transaction');
 });
