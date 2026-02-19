cecfab3090b1db7874ce4cb5b5039faf9cfa0cb9
shafaadha <adhashafa@gmail.com>
2026-02-20 00:17:26 +0700

    feat: add @role directive for role-based access

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Blade;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +20,12 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Blade::directive('role', function ($role) {
+            return "<?php if(auth()->check() && auth()->user()->role == $role): ?>";
+        });
+
+        Blade::directive('endrole', function () {
+            return "<?php endif; ?>";
+        });
     }
 }
